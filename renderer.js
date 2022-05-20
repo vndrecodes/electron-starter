16424cc5fd45fa64e0595090f7ebf9c480152ce2
André <25619983+vndrecodes@users.noreply.github.com>
2022-05-20 22:08:15 +0200

    minor change

--- renderer.js
+++ renderer.js
@@ -2,7 +2,6 @@ window.electronAPI.send('unidirectional:channel', 'Hey there!');
 
 (async () => {
   let result = await window.electronAPI.invoke('bidirectional:channel', 'Talk to me!');
+  console.log(result);
+  console.log('Find me @ the windows DevTools console!');
 })();
-
-
-console.log('Find me @ the windows DevTools console!');
